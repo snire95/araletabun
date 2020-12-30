f4c8a8aecc79fd662e31513ccba624e4dfbaa8e3
snire95 <snire995@gmail.com>
2020-12-30 02:11:25 +0200

    araleTabun

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = ['כל המנות',...new Set(items.map((item) => item.category))]
+console.log(allCategories)
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === 'כל המנות'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item)=> item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className= 'title'>
+          <h1>ארל'ה פיצה</h1>
+          <h2>התפריט שלנו</h2>
+          <div className='underline'></div>       
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items ={menuItems}/>
+      </section>
+    </main>
+  )
+}
+
+export default App;
